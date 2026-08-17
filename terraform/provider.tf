43bc4e4b4957f887e7d3920e4907e5141140db78
thebasedtaka <micah.philpotts@gmail.com>
2026-08-16 23:10:03 -0400

    I broke everything

---
+++ terraform/provider.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=4.12.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.api_fingerprint
+  private_key_path = var.api_private_key_path
+  region           = var.region
+}
+
+provider "oci" {
+  alias            = "home"
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.api_fingerprint
+  private_key_path = var.api_private_key_path
+  region           = var.region
+}
+
+module "k8s_setup" {
+  source = "ystory/always-free-oke/oci"
+
+  tenancy_id     = var.tenancy_id
+  home_region    = var.region
+  region         = var.region
+  node_pool_size = 1
+
+  ssh_public_key_path  = "~/.ssh/id_rsa.pub"
+  ssh_private_key_path = "~/.ssh/id_rsa"
+
+  providers = {
+    oci.home = oci.home
+  }
+  kubernetes_version = "v1.36.1"
+  node_pool_os_version = "8.10"
+}
